58c5869a68464a810ce35323f217c44471b3ec86
Victor Cojocaru <victorcojocaru93@gmail.com>
2019-03-04 16:28:15 -0600

    test commit

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/api/user',user);
 //Serve static assets if we are in production
 if(process.env.NODE_ENV === 'production'){
     //set a static folder
-    app.use(express.static('react-app/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'react-app', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
